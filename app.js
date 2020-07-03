174c4695b6a6c8c1a4551f9f662552b6a792fe16
bingyilin1472 <67002138+bingyilin1472@users.noreply.github.com>
2020-07-04 07:44:53 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+new Vue({
+    el: '#app',
+    data: {
+        title: 'Vue forLooping v-for',
+        //裡面可以有dict or list構成nested structure
+        items: ['Vegetable', 'Kiwi fruit', 'Apple', 'Banana', 'Star'],
+        ninjas:[
+            {name: 'Tim', age: 25, belt: 'Black'},
+            {name: 'Crystal', age: 30, belt: 'Red'},
+            {name: 'Ken', age: 35, belt: 'Orange'}
+        ]
+    },
+    methods: {
+
+    }
+})
